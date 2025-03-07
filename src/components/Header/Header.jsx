afbc001b25f14a7ad99ba9368608d18de4909224
Aniket Gawande <146917796+Aniketgawande1@users.noreply.github.com>
2025-03-08 03:27:04 +0530

    hero added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React from "react";
+import logo from "/logo.png";
+import './Header.css'
+// import 'src/index.css'
 
 const Header = () => {
   return (
-    <section className='h-wrapper'>
-        <div className=" flexCenter paddings innerWidth h-container">
-            <img src="./logo.png" alt="logo"  width={100}/>
+    <section className="h-wrapper">
+      <div className="flexCenter h-container">
+        <img src={logo} alt="logo" width={100} />
 
-            <div className="h-menu">
-              <a href="">Resideance</a>
-              <a href="">Our Value</a>
-              <a href="">Contact Us</a>
-              <a href="">Get Started</a>
-              <button className='button'><a href="">Contact</a>
-              </button>
-              
-            </div>
-        </div>
+        <div className="h-menu flexCenter">
+  <a href="#">Residence</a>
+  <a href="#">Our Value</a>
+  <a href="#">Contact Us</a>
+  <a href="#">Get Started</a>
+  <button className="button">
+    <a href="#">Contact</a>
+  </button>
+</div>
+
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import "./Hero.css"
 
 const Hero = () => {
   return (
     <div>
-      
+      <section className="hero-wrapper">
+        <div className="paddings innerwidth flexCenter hero-container ">
+
+        <div className= "hero-left">
+              
+         </div>
+         <div className= "flexCenter hero-right">
+          <div className="image-container">
+            <img src="./hero-image.png" alt="" />
+          </div>
+         </div>
+
+        </div>
+      </section>
     </div>
   )
 }
